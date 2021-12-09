02df237c83845835034b29febfab3c521f9eddcf
JohnPac <johnpac15@yahoo.com>
2021-12-08 23:21:01 -0600

    add the panet info from planet API

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var subBtn = document.getElementById("sub-btn")
 var cityInput = document.getElementById("city-finder")
 
+var goodieDiv = document.getElementById("all-the-goodies")
+
 
 getCityDetails = function(C) {
     let getThoseDetails = "https://api.openweathermap.org/data/2.5/weather?q=" + C + "&units=imperial&appid=d310cdc3e7de424fc0047cf1fd72fd27";
@@ -20,13 +22,13 @@ getCityDetails = function(C) {
                 let sunRiseFullTime = new Date(sunRiseTimeData * 1000)
                 let sunRiseTimeHour = sunRiseFullTime.getHours()
                 let sunRiseTimeMinute = sunRiseFullTime.getMinutes()
-                console.log(sunRiseTimeHour + ":" +sunRiseTimeMinute)
+                let sunRiseShowTime = sunRiseTimeHour + ":" +sunRiseTimeMinute
                 //Sunset time
                 let sunSetTimeData = data.sys.sunset
                 let sunSetFullTime = new Date(sunSetTimeData * 1000)
                 let sunSetTimeHour = sunSetFullTime.getHours()
                 let sunSetTimeMinute = sunSetFullTime.getMinutes()
-                console.log(sunSetTimeHour + ":" +sunSetTimeMinute)
+                let sunSetShowTime = sunSetTimeHour + ":" +sunSetTimeMinute
 
                 //Clouds Info
                 let cloudData = data.weather[0].description
@@ -43,7 +45,7 @@ getCityDetails = function(C) {
                     if(response.ok) {
                         response.json().then(function(moreData) {
                             console.log(moreData)
-
+                            goodieDiv.textContent = ""
                             //Moon Phase
                             let moonPhase = moreData.daily[0].moon_phase
 
@@ -62,7 +64,30 @@ getCityDetails = function(C) {
                             let moonSetMinute = moonSetFullTime.getMinutes()
                             console.log(moonSetHour + ":" + moonSetMinute)
 
+                            weatherDiv = document.createElement("div")
+                            weatherDiv.setAttribute("style", "border: 3px solid red")
+                            goodieDiv.appendChild(weatherDiv)
 
+                            pOne = document.createElement("p")
+                            pTwo = document.createElement("p")
+                            pThree = document.createElement("p")
+                            pFour = document.createElement("p")
+                            pFive = document.createElement("p")
+
+                            pOne.textContent = "Your GPS coordanites: " + lat + "," + lon
+                            pTwo.textContent = "Sunrise Time : " + sunRiseShowTime
+                            pThree.textContent = "Sunset Time : "+ sunSetShowTime
+                            pFour.textContent = "Cloud Information: " +cloudData
+                            pFive.textContent = "Moon Phase: " + moonPhase
+
+                            weatherDiv.appendChild(pOne)
+                            weatherDiv.appendChild(pTwo)
+                            weatherDiv.appendChild(pThree)
+                            weatherDiv.appendChild(pFour)
+                            weatherDiv.appendChild(pFive)
+                            
+                            
+                            getPlanetInfo(lat, lon);
 
                             
 
@@ -89,3 +114,47 @@ subBtn.addEventListener("click", function(event) {
 
     getCityDetails(cityName)
 })
+
+
+getPlanetInfo = function(latVar, lonVar) {
+    let planetInfo = "https://visible-planets-api.herokuapp.com/v2?latitude="+latVar+"&longitude="+lonVar+"&showCoords=true"
+
+    fetch(planetInfo).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(planetData) {
+                var planets = planetData.data
+                console.log(planets)
+                planetDiv = document.createElement("div")
+                goodieDiv.appendChild(planetDiv)
+                planetDiv.setAttribute("style", "border: 3px solid black;")
+                
+                for(i=0;i<planets.length; i++) {
+                    let planetName = planets[i].name
+                    let planetRiseHour= planets[i].rightAscension.hours
+                    let planetRiseMinutes= planets[i].rightAscension.minutes
+                    let planetRiseSeconds= planets[i].rightAscension.seconds
+                  
+
+                    h2El = document.createElement("h2")
+                    p2El = document.createElement("p")
+                    p3El = document.createElement("p")
+                    
+
+                    h2El.textContent = "Planet Name: " + planetName
+                    p2El.textContent = planetName+ " Rise time: " + planetRiseHour + ":" + planetRiseMinutes + ":" + planetRiseSeconds
+                    p3El.textContent = "stop"
+
+                    planetDiv.appendChild(h2El)
+                    planetDiv.appendChild(p2El)
+                    planetDiv.appendChild(p3El)
+
+
+
+                }
+
+
+                
+            })
+        }
+    })
+}
